759e7379496749cfdd34ca2fd1da2feef76b4f67
mikekuanhsuan <91623101+mikekuanhsuan@users.noreply.github.com>
2021-12-13 21:45:51 +0800

    Update test-f.py

--- test-f.py
+++ test-f.py
@@ -1,21 +1,6 @@
-const http = require('http');
-const fs = require('fs');
+PORT=3001
 
-const server = http.createServer((req, res)=>{
-    console.log('---a');
-    fs.writeFile(
-        __dirname + '/headers01.txt',
-        JSON.stringify(req.headers),
-        (error)=>{
-            if(!error){
-                console.log('---b');
-                res.end('ok');
-            } else {
-                console.log('---c');
-                res.end(error);
-            }
-        });
-    console.log('---d');
-});
-
-server.listen(3000);
+DB_HOST=localhost
+DB_USER=root
+DB_PASS=Aa99405Mike012@
+DB_NAME=proj57
